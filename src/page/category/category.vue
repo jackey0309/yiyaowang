71c87aa6a0007eb0dd275b89e47e54d6ed1f1de7
duxingxia <543548977@qq.com>
2019-02-19 00:03:49 +0800

    category 70% completed

--- src/page/category/category.vue
+++ src/page/category/category.vue
@@ -1,14 +1,101 @@
 <template>
-    <div>
-        <h1>category</h1>
-        <foot-guide></foot-guide>   
+    <div class="page_categories">
+        <div class="categories">
+            <!-- header_bar -->
+        <head-top>
+            <header slot="header" class="categories_header_bar">
+                <div class="categories_header_con">
+                    <a href="javascript:void(0)" class="categories_header_left_btn">
+                        <i class="logo"></i>
+                    </a>
+                    <span class="address_map">上海</span>
+                    <div class="categories_header_search">
+                        <div class="search_box">
+                            <i class="icon iconfont icon-sousuo"></i>
+                            输入商品名称
+                        </div>
+                    </div>
+                    <a href="" class="categories_header_right_btn">
+                        <i class="tel"></i>
+                    </a>
+                </div>
+            </header>
+        </head-top>
+        <!-- categories_banner -->
+        <div class="categories_banner">
+            <a href="https://yzm.111.com.cn/m/yw-twwz/index.html?channel=yiyaoapp">
+            <img src="http://m.111.com.cn/yyw/wap/category/static/img/categorie_bannr.de2a6a8.png" alt=""></a>
+        </div>
+        <!-- categories_content -->
+        <div class="categories_content">
+            <ul class="categories_list">
+                <li>
+                    <a href="javascript:;" class="active">热门类目</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+                <li>
+                    <a href="javascript:;">男科用药</a>
+                </li>
+            </ul>
+            <div class="categories_box">
+
+            </div>
+        </div>
+        <foot-guide></foot-guide>  
+        </div> 
     </div>
 </template>
 
 <script>
+import headTop from '../../components/header/head'
 import footGuide from '../../components/footer/footGuide'
 export default {
       components:{
+         headTop,  
         footGuide
     },
 }
@@ -16,4 +103,159 @@ export default {
 
 <style lang="scss" scoped>
  @import '../../style/mixin';
+ .categories, .page_categories {
+    height: 100%;
+    overflow: hidden;
+}
+.categories {
+    margin: 0 auto;
+    padding-bottom: 1.4666666666666666rem;
+    padding-top: 1.1733333333333333rem;
+}
+/*header_bar*/
+.categories_header_bar {
+    height: 1.1733333333333333rem;
+    background: #f9f9f9;
+    border: 1px solid #e7e7e7;
+    box-sizing: border-box;
+    position: fixed;
+    width: 100%;
+    z-index: 100;
+    left: 0;
+    top: 0;
+    text-align: center;
+}
+.categories_header_bar .categories_header_con {
+    position: relative;
+    padding: 0 1.3333333333333333rem 0 3.2rem;
+    height: 1.1733333333333333rem;
+}
+.categories_header_bar .categories_header_con .categories_header_left_btn {
+    padding: 0 .4rem;
+    height: 1.1733333333333333rem;
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    line-height: 1.1733333333333333rem;
+}
+.categories_header_bar .categories_header_con .logo {
+    margin-top: .21333333333333335rem;
+    width: .6666666666666666rem;
+    height: .7466666666666667rem;
+    background: url(../../images/logo.png) no-repeat;
+    display: block;
+    background-size: .6666666666666666rem auto;
+}
+.categories_header_bar .categories_header_con .address_map {
+    position: absolute;
+    line-height: 1.1733333333333333rem;
+    left: 1.3333333333333333rem;
+    top: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 1.2rem;
+}
+.categories_header_bar .categories_header_con .categories_header_search {
+    position: absolute;
+    right: 1.6rem;
+    left: 2.6666666666666665rem;
+}
+.categories_header_bar .categories_header_con .categories_header_right_btn {
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 1.1733333333333333rem;
+    line-height: 1.1733333333333333rem;
+    display: inline-block;
+    padding: 0 .26666666666666666rem;
+    cursor: pointer;
+}
+.categories_header_bar .categories_header_con .categories_header_search .search_box {
+    height: .6666666666666666rem;
+    line-height: .6666666666666666rem;
+    margin-top: .26666666666666666rem;
+    font-size: .32rem;
+    padding: 0 .26666666666666666rem;
+    text-align: left;
+    border-radius: 10px;
+    background: #ebebeb;
+    color: #b8b8be;
+    cursor: text;
+}
+.categories_header_bar .categories_header_con .categories_header_search .iconfont {
+    font-size: .4rem;
+    color: #b8b8be;
+    margin-right: .16rem;
+}
+.categories_header_bar .categories_header_con .tel {
+    width: 1.2rem;
+    height: .6666666666666666rem;
+    line-height: .6666666666666666rem;
+    margin-top: .21333333333333335rem;
+    display: block;
+    background: url(../../images/tel-category.png) no-repeat;
+    background-size: 1.2rem auto;
+    background-position: 0;
+}
+/*header_bar*/
+/*categories_banner*/
+.categories_banner, .categories_banner img {
+    height: 1.1733333333333333rem;
+}
+.categories_banner img {
+    width: 100%;
+    vertical-align: middle;
+}
+.categories_banner, .categories_banner img {
+    height: 1.1733333333333333rem;
+}
+/*categories_banner*/
+
+/*categories_content*/
+.categories_content {
+    height: 100%;
+    zoom: 1;
+    margin: 0 auto;
+    overflow: hidden;
+    font-size: .37333333333333335rem;
+}
+.categories_content .categories_list {
+    height: 100%;
+    overflow: auto;
+    padding-bottom: 1.1733333333333333rem;
+    width: 25%;
+    float: left;
+    -webkit-overflow-scrolling: touch;
+    border-right: 1px solid rgba(0,0,0,.1);
+}
+.categories_content .categories_box {
+    width: 75%;
+    height: 100%;
+    overflow-y: auto;
+    padding: 0 0 0 .26666666666666666rem;
+    background: #fff;
+}
+.categories_content .categories_list li {
+    height: 1.3066666666666666rem;
+    border-bottom: 1px solid rgba(0,0,0,.1);
+}
+.categories_content .categories_list li a.active {
+    color: #f66;
+    background: #fff;
+    border-left: 2px solid #f66;
+}
+.categories_content .categories_list li a {
+    height: 1.3066666666666666rem;
+    line-height: 1.3066666666666666rem;
+    padding: 0 .26666666666666666rem;
+    color: #333;
+    display: block;
+    overflow: hidden;
+}
+
+
+/*categories_content*/
+
 </style>
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,7 +1,22 @@
 <template>
     <div id="home" class="pre-home page">
          <div class="header_cover" style="opacity: 0; transition: all 0.5s ease 0s;"></div>
-       <head-top></head-top>
+       <head-top>
+            <header slot="header" class="header">
+                <div class="header-searchBar">
+                    <div class="logo"></div>
+                    <div class="address_map ellipsis" >上海</div>
+                    <div class="searchbar">
+                        <a href="http://m.111.com.cn/yyw/wap/searchList/#/search" class="search_box">
+                            <i class="icon iconfont icon-sousuo"></i>
+                            输入商品名称
+                        </a>
+                        <a href="tel:4000070958" class="tel"></a>
+                    </div>
+                    <div class="tel"></div>
+                </div>
+            </header>
+       </head-top>
         <div class="content">
                 <!-- swipe -->
             <mt-swipe :stopPropagation="true" :prevent="true" :auto="5000" class="banner">
@@ -941,6 +956,84 @@ export default {
         width: 100%;
         line-height: 1.2rem;
     }
+   /* header */
+   .header{
+        font-size: .4rem;
+        text-align: center;
+    }
+    .header{
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 8;
+        width: 100%;
+        line-height: 1.2rem;
+    }
+     .header-searchBar{
+        position: relative;
+    }
+    .header .logo{
+        background: url(../../images/logo.png) no-repeat .4rem;
+        left: 0;
+        width: 1.33333333rem;
+        background-size: auto .74666667rem; 
+        }
+    .header .address_map{
+        left: 1.2rem;
+        padding-top: .10666667rem;
+        width: 1.2rem;
+        line-height: .53333333rem;
+        font-size: .37333333rem;
+        color: #fff;
+    }
+    .header .logo , .header .address_map{
+        position: absolute;
+        top: 0;
+        height: .64rem;
+        display: block;
+    }
+    .header .searchbar{
+        padding: 0 1.73333333rem 0 2.4rem;
+        color: #a7a7a7;
+        .search_box{
+            height: .74666667rem;
+            line-height: .74666667rem;
+            font-size: .32rem;
+            margin-top: .26666667rem;
+            padding: 0 .26666667rem;
+            text-align: left;
+            // CSS3文档中提到：（HSLA）
+            // H是色度，取值在0度~360度之间，0度是红色，120度是绿色，240度是蓝色。360度也是红色。
+            // S是饱和度，是色彩的纯度，是一个百分比的值，取值在0%~100%，0%饱和度最低，100%饱和度最高
+            // L是亮度，也是一个百分比值，取值在0%~100%，0%最暗，100%最亮。
+            // A是不透明度，取值在0.0~1.0，0.0完全透明，1.0完全不透明。
+            background: hsla(0,0%,100%,.9);   
+            border-radius: 10px;
+            color: #898989;
+            box-sizing: border-box;
+            display: block;
+            width: 100%;
+        }
+       .iconfont {
+            font-size: .42666667rem;
+            margin-right: .16rem;
+            color: #898989;
+            } 
+    }
+    .header .tel {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 1.22666667rem;
+        height: 100%;
+        line-height: .4rem;
+        padding-left: .26666667rem;
+        background: url(../../images/tel.png) no-repeat .05333333rem;
+        background-size: 1.22666667rem auto;
+    }
+
+
+ /* header */
  /* swipe */
   .banner {
     height: 3.97333333rem;
